05d3092ca5fdcd2b8cfd0a9ef8a8e89f2bc941cd
Viki <91424361+viki441@users.noreply.github.com>
2025-03-12 12:29:30 +0200

    Update 21_1task.hs

--- 22 section/21_1task.hs
+++ 22 section/21_1task.hs
@@ -1,12 +1,9 @@
-triangleArea :: Double -> Double -> Double
-triangleArea side h = (side * h) / 2
-
-triangleArea2 :: Double -> Double -> Double -> Double
-triangleArea2 a b c = sqrt (s * (s - a) * (s - b) * (s - c))
+triangleAreaList :: [Double] -> Double
+triangleAreaList [base, height] = (base * height) / 2 
+triangleAreaList [a, b, c] = sqrt (s * (s - a) * (s - b) * (s - c)) 
   where
-    s = (a + b + c) / 2 
-
+    s = (a + b + c) / 2
 main :: IO ()
 main = do
   print(triangleArea 2.5 4)
-  print(triangleArea2 2.0 4.0 6.0)
+  print(triangleArea 2.0 4.0 6.0)
