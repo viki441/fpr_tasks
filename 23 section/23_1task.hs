7a8d21d8a41ccd4ee1c12de995b32cc6825de3f4
Viki <91424361+viki441@users.noreply.github.com>
2025-03-16 23:36:36 +0200

    Update 23_1task.hs

--- 23 section/23_1task.hs
+++ 23 section/23_1task.hs
@@ -2,6 +2,13 @@ findLength :: [a] -> Int
 findLength [] = 0
 findLength (_:xs) = 1 + findLength xs
 
+findElem :: Eq a => a -> [a] -> Bool
+findElem _ [] = False
+findElem n (x:xs)
+  | n == x = True
+  |otherwise = findElem n xs
+
 main :: IO ()
 main = do
   print(findLength ["a", "d"])
+  print(findElem 5 [1,2,3,4])
