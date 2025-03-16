4c47dc9aa31e282597138605ac3afee8e6e3707a
Viki <91424361+viki441@users.noreply.github.com>
2025-03-16 20:35:34 +0200

    Update 22_10task.hs

--- 22 section/22_10task.hs
+++ 22 section/22_10task.hs
@@ -1,12 +1,9 @@
-import Prelude
-isPower :: Double -> Double -> Bool
-isPower 0 0 = True
-isPower _ 0 = False
-isPower 0 _ = False
-isPower x k
-  | x == 1 = True
-  | x / k == fromIntegral (floor (x / k)) = isPower (x / k) k
-  |otherwise = False
+isPower :: Int -> Int -> Bool
+isPower 1 _ = True
+isPower n k
+  | n < 1 || k <= 1 = False 
+  | n `mod` k /= 0 = False
+  | otherwise = isPower (n `div` k) k
 
 main :: IO ()
 main = do 
