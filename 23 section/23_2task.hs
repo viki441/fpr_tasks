5088154a13e41c8c8c060047e439dd6b80609f5c
Viki <91424361+viki441@users.noreply.github.com>
2025-03-17 08:07:52 +0200

    Update 23_2task.hs

--- 23 section/23_2task.hs
+++ 23 section/23_2task.hs
@@ -1,8 +1,14 @@
-countElem :: Eq a => a -> [a] -> Int
+{-countElem :: Eq a => a -> [a] -> Int
 countElem _ [] = 0
 countElem y (x:xs)
   |y == x = 1 + countElem y xs
   |otherwise = countElem y xs
+-}
+countElem :: Eq a => a -> [a] -> Int
+countElem _ [] = 0
+countElem y l
+  |y == (head l) = 1 + countElem y (tail l)
+  |otherwise = countElem y (tail l)
 
 main :: IO ()
 main = do
