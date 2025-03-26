b2ff2b1e4cc1dd6422ca1864c715bd5abcb65600
Viki <91424361+viki441@users.noreply.github.com>
2025-03-26 16:08:00 +0200

    Update 02simpleDecode.hs

--- week 03/02simpleDecode.hs
+++ week 03/02simpleDecode.hs
@@ -1,6 +1,12 @@
 simpleDecode :: [(Char, Int)] -> String
 simpleDecode xs = [ ch | (ch, n) <- xs, _ <- [1..n] ]
 
+arProgession :: Int -> Int -> Int -> [Int]
+arProgession n a d = [a + (i * d) | i <- [0..(n-1)]]
+
+
 main :: IO ()
 main = do
     print(simpleDecode [('a',2),('b',4)])
+    print (arProgession 5 1 2)
+
