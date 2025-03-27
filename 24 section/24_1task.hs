52a78ee2aa2ee2c8834f91cf3b4c0922786da849
Viki <91424361+viki441@users.noreply.github.com>
2025-03-27 10:25:25 +0200

    Update 24_1task.hs

--- 24 section/24_1task.hs
+++ 24 section/24_1task.hs
@@ -1,6 +1,10 @@
 firstNEvens :: Int -> [Int]
 firstNEvens n = take n [x | x <- [2..], x `mod` 2 == 0]
 
+arProgession :: Int -> Int -> Int -> [Int]
+arProgession n a d = [a + (i * d) | i <- [0..(n-1)]]
+
+
 main :: IO ()
 main = do
     print (firstNEvens 10)  
