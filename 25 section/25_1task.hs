03e7fb74115e20af33378a22b4429922775b8130
Viki <91424361+viki441@users.noreply.github.com>
2025-03-31 11:25:03 +0300

    Update and rename 01task.cpp to 25_1task.hs

---
+++ 25 section/25_1task.hs
@@ -0,0 +1,7 @@
+say :: Int -> String
+say n 
+    | n >= 0 && n <= 9 = words "zero one two three four five six seven eight nine" !! n
+    | otherwise = "Invalid input"
+
+main :: IO ()
+main = print (say 3)
