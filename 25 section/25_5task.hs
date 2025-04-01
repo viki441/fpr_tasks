f1664ce3434878fbba59bcc4c5aeb6502cbfc9fa
Viki <91424361+viki441@users.noreply.github.com>
2025-04-01 16:48:50 +0300

    Update 25_5task.hs

--- 25 section/25_5task.hs
+++ 25 section/25_5task.hs
@@ -1,10 +1,19 @@
 toUpperCase :: Char -> Char
 toUpperCase c = if c >= 'a' && c <= 'z' 
-                then toEnum (fromEnum c - 32) 
+                --Converts an enumerated type (like Char) into an Int representation
+                then toEnum (fromEnum c - fromEnum 'a' + fromEnum 'A') :: Char 
+                --Char:: It tells Haskell that the result of toEnum should be interpreted as a Char.
                 else c
 
 convertString :: String -> String
 convertString = map toUpperCase
+
+--OR
+import Data.Char (toUpper)
+
+convertString :: String -> String
+convertString = map toUpper
+
 
 main :: IO ()
 main = do
