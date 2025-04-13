e012f5df0fa7e66e491694e400daa291dd96065e
Viki <91424361+viki441@users.noreply.github.com>
2025-04-13 20:29:20 +0300

    Update and rename 03div3notdiv7 to 03div3notdiv7.hs

--- week 04/03div3notdiv7.hs
+++ week 04/03div3notdiv7.hs
@@ -1,5 +1,7 @@
 div3notdiv7 :: [Int] -> [Int]
 div3notdiv7 = filter (\x -> x `mod` 3 == 0 && x `mod` 7 /= 0)
+--also works: div3notdiv7 list = filter (\x -> x `mod` 3 == 0 && x `mod` 7 /= 0) list
+
 
 main :: IO ()
 main = do
